cea9914f3e93366819f686f2c31f883ce2aff6a3
abdulmabud <abdulmabudm@gmail.com>
2020-07-18 13:59:02 +0600

    Order statistics show in dashboard

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\OrderStatus;
 use Auth;
+
 class BackendController extends Controller
 {
     public function index(){
-        return view('backend.dashboard');
+        $data['pending'] = OrderStatus::where('status', 'Pending')->count();
+        $data['accepted'] = OrderStatus::where('status', 'Accepted')->count();
+        $data['pickedUp'] = OrderStatus::where('status', 'Picked Up')->count();
+        $data['inProgress'] = OrderStatus::where('status', 'In Progress')->count();
+        $data['rescheduled'] = OrderStatus::where('status', 'Rescheduled')->count();
+        $data['done'] = OrderStatus::where('status', 'Done')->count();
+ 
+        return view('backend.dashboard', $data);
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -5,5 +5,48 @@
 @endsection
 
 @section('content')
-    
+    <div class="container">
+        <h3 class="text-primary mb-3">Order Statistics</h3>
+        <select name="" id="" class="form-control mb-4 w-25">
+            <option value="">All Time</option>
+        </select>
+        <div class="row">
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #aabd0e;">Pending
+                    <br>
+                    <span class="text-light bold h3">{{ $pending }}</span>
+                </p>
+            </div>
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #b870bd;">Accepted
+                    <br>
+                    <span class="text-light bold h3">{{ $accepted }}</span>
+                </p>
+            </div>
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #49b387;">Picked Up
+                    <br>
+                    <span class="text-light bold h3">{{ $pickedUp }}</span>
+                </p>
+            </div>
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #54984f;">In Progress
+                    <br>
+                    <span class="text-light bold h3">{{ $inProgress }}</span>
+                </p>
+            </div>
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #0ebd85;">Rescheduled
+                    <br>
+                    <span class="text-light bold h3">{{ $rescheduled }}</span>
+                </p>
+            </div>
+            <div class="col-md-2 text-center dash-block">
+                <p class="py-5 px-1 text-light h4" style="background-color: #0ebd85;">Done
+                    <br>
+                    <span class="text-light bold h3">{{ $done }}</span>
+                </p>
+            </div>
+        </div>
+    </div>
 @endsection
